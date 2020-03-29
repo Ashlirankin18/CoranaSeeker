0494dbdb22b4b2e616911a4573f9a471ae286157
AshliRankin <ashlirankin18@gmail.com>
2020-03-29 10:22:38 -0400

    Added an xcode project

---
+++ CoranaSeeker/CoranaSeeker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoranaSeeker
+//
+//  Created by Ashli Rankin on 3/29/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
