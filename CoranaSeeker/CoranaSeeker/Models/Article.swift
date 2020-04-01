019d7310677bfab7fee27b1426a46726cab001a9
AshliRankin <ashlirankin18@gmail.com>
2020-04-01 13:49:27 -0400

    Created an article model

---
+++ CoranaSeeker/CoranaSeeker/Models/Article.swift
@@ -0,0 +1,26 @@
+//
+//  Article.swift
+//  CoranaSeeker
+//
+//  Created by Ashli Rankin on 4/1/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleResult: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let author: String
+    let title: String
+    let description: String
+    let url: URL
+    let imageURL: URL
+    
+    private enum CodingKeys: String, CodingKey {
+        case author, title, description, url
+        case imageURL = "urlToImage"
+    }
+}
